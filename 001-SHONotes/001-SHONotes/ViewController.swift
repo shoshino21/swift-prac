d27af85d5692d1981f044ba232eee1a2660901d3
雲端開發部-江世豪 <shoshino@mitake.com.tw>
2019-02-14 17:37:13 +0800

    001 init

---
+++ 001-SHONotes/001-SHONotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  001-SHONotes
+//
+//  Created by 雲端開發部-江世豪 on 2019/2/14.
+//  Copyright © 2019 com.mitake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+}
+
